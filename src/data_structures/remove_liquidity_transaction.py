b6c7765ed8245e2ab6673557d5ccde50bac367ca
bout3fiddy <11488427+bout3fiddy@users.noreply.github.com>
2021-07-11 17:52:49 +0200

    update remove liq method

--- src/data_structures/remove_liquidity_transaction.py
+++ src/data_structures/remove_liquidity_transaction.py
@@ -1,4 +1,6 @@
 from src.data_structures.core.transaction import Transaction
+from src.utils.constants import TRICRYPTO_LP_TOKEN_ADDR
+from src.utils.exceptions import IncorrectContractException
 
 
 class RemoveLiquidityTransaction(Transaction):
@@ -6,6 +8,22 @@ class RemoveLiquidityTransaction(Transaction):
     def __init__(self, transaction_hash: str):
 
         super().__init__(transaction_hash=transaction_hash)
+        self.removed_lp_tokens = 0
+        self.get_removed_lp_tokens()
+
+    def get_removed_lp_tokens(self):
+
+        if self.logs_are_processed:
+            return self.minted_lp_tokens
+
+        log_to_process = self.tx_receipt["logs"][0]
+        try:
+            processed_log = TRICRYPTO_LP_TOKEN_ADDR.events.Transfer().processLog(log_to_process)
+        except Exception as e:
+            raise IncorrectContractException
+
+        self.removed_lp_tokens = processed_log["args"]["_value"]
+        self.logs_are_processed = True
 
 
 def main():
